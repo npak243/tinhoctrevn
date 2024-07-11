f747ed621c702669c9092ad6b3b590e281a0cc02
Khoa Nguyen <Khoa.Nguyen@washpost.com>
2024-07-11 17:38:36 +0700

    phan_so_nho_nhat

---
+++ python/phan_so_nho_nhat.py
@@ -0,0 +1,21 @@
+a = int(input())
+b = int(input())
+c = int(input())
+
+numerator = min(a, b, c)
+denominator = max(a, b, c)
+
+if denominator % numerator == 0:
+    result = (denominator // numerator) + 1
+elif (
+    (denominator % (denominator - numerator) == 0) and
+    (numerator % (denominator - numerator) == 0)
+):
+    result = int(
+        (denominator // (denominator - numerator)) +
+        (numerator // (denominator - numerator))
+    )
+else:
+    result = denominator + numerator
+
+print(result)
